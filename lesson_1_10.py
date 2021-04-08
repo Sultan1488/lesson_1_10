520f261523171f8004da065927bc157796a7bd3c
Sultan <sultan@MacBook-Pro.local>
2021-04-08 21:30:35 +0600

    add def

--- lesson_1_10.py
+++ lesson_1_10.py
@@ -1,13 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-url = 'https://habr.com/ru/search/?q=python#h'
-
-source = requests.get(url)
-main_text = source.text
-soup = BeautifulSoup(main_text, "html.parser")
-
-link = soup.find('a', {'class': 'post__title_link'})
-
-print(link.text)
+def find(url, teg, class_):
+    source = requests.get(url)
+    main_text = source.text
+    soup = BeautifulSoup(main_text, "html.parser")
+    link = soup.find(teg, {'class': class_})
+    return link.text
+find('https://habr.com/ru/search/?q=python#h', 'a', 'post__title_link')
+find('https://career.habr.com/vacancies?type=all', 'div', 'vacancy_banner__title')
+find('https://www.ts.kg/show/python_razrabotka_s_nulya_do_professionala', 'div', 'app-show-header')
